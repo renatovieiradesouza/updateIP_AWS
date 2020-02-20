d980a9d7be7033c16e62102dd9938277eb732f09
kelweenn.praes <kelweenn.praes@dextra-sw.com>
2020-02-20 16:21:03 -0300

    Updating

--- sendMyIP.sh
+++ sendMyIP.sh
@@ -1,3 +1,6 @@
 MyIP=`dig +short myip.opendns.com @resolver1.opendns.com`
 username=`git config --get user.email`
 echo "$MyIP/32;$username" > $username.dados
+git add $username.dados
+git commit -m "$username; IP: $MyIP"
+git push
