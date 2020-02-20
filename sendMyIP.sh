f17788854e81b85acd04991c81d006192716ee9e
kelweenn.praes <kelweenn.praes@dextra-sw.com>
2020-02-20 16:57:03 -0300

    Corrections

--- sendMyIP.sh
+++ sendMyIP.sh
@@ -1,3 +1,8 @@
+set -x
+	rm -f ./*.dados
+	git add .
+	git commit -m "Corrections $username"
+	git push
 MyIP=`dig +short myip.opendns.com @resolver1.opendns.com`
 username=`git config --get user.email`
 echo "$MyIP/32;$username" > $username.dados
