342ef51981cda5d67e4fed957371442377ffe8b9
kelweenn.praes <kelweenn.praes@dextra-sw.com>
2020-02-20 14:38:05 -0300

    The Beginning

---
+++ updateMyIP.sh
@@ -0,0 +1,7 @@
+#set -x
+MyIP=`dig +short myip.opendns.com @resolver1.opendns.com`
+raw_command=`echo -e "/usr/local/bin/aws ec2 authorize-security-group-ingress --group-id sg-0cebfa1c3c902e7fe --ip-permissions IpProtocol=-1,FromPort=-1,ToPort=-1,IpRanges='[{CidrIp=$MyIP/32,Description="Meu IP"}]'"`
+full_command=`echo "eval $raw_command"`
+command=`echo $full_command|sed 's/eval //g'`
+eval "${command}"
+
